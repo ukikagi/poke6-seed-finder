9187ec8b8d514c1a66b25996070bb58a5898f8c5
ukikagi <ukikagi@gmail.com>
2023-09-11 02:55:55 +0900

    Use BM method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use boyer_moore_magiclen::byte::BMByte;
 use indicatif::ParallelProgressIterator;
 use mersenne_twister::MT19937;
 use rand::Rng;
@@ -11,57 +12,54 @@ type IV = (u8, u8, u8, u8, u8, u8);
 type Frame = usize;
 type Seed = u32;
 
-fn find_frame(rand_pool: &[u8], iv: IV, (frame_min, frame_max): (Frame, Frame)) -> Option<Frame> {
-    for i in frame_min..=frame_max {
-        if rand_pool[i + 0] == iv.0
-            && rand_pool[i + 1] == iv.1
-            && rand_pool[i + 2] == iv.2
-            && rand_pool[i + 3] == iv.3
-            && rand_pool[i + 4] == iv.4
-            && rand_pool[i + 5] == iv.5
-        {
-            return Some(i);
-        }
+fn discard(rng: &mut MT19937, n: usize) {
+    for _ in 0..n {
+        rng.next_u32();
     }
-    None
 }
 
 fn find_frame_pair(
     seed: Seed,
-    iv1: IV,
-    iv2: IV,
-    frame_range1: (Frame, Frame), // right-inclusive
-    frame_range2: (Frame, Frame), // right-inclusive
+    bmb1: &BMByte,
+    bmb2: &BMByte,
+    (f1_min, f1_max): (Frame, Frame), // right-exclusive
+    (f2_min, f2_max): (Frame, Frame), // right-exclusive
 ) -> Option<(Seed, Frame, Frame)> {
     let mut mt = MT19937::from_seed(seed);
-    for _ in 0..PRE_ADVANCE_FRAME {
-        mt.next_u32();
-    }
+    discard(&mut mt, PRE_ADVANCE_FRAME + f1_min);
 
-    let pool_size = frame_range2.1 + 6;
-    let mut rand_pool: Vec<u8> = Vec::with_capacity(pool_size);
-    for _ in 0..pool_size {
+    let mut rand_pool: Vec<u8> = Vec::with_capacity(f1_max - f1_min);
+    for _ in f1_min..f1_max {
         rand_pool.push((mt.next_u32() >> 27) as u8)
     }
+    let f1 = f1_min + bmb1.find_first_in(&rand_pool)?;
 
-    Some((
-        seed,
-        find_frame(&rand_pool, iv1, frame_range1)?,
-        find_frame(&rand_pool, iv2, frame_range2)?,
-    ))
+    discard(&mut mt, f2_min - f1_max);
+
+    rand_pool.clear();
+    rand_pool.reserve(f2_max - f2_min);
+    for _ in f2_min..f2_max {
+        rand_pool.push((mt.next_u32() >> 27) as u8)
+    }
+    let f2 = f2_min + bmb2.find_first_in(&rand_pool)?;
+
+    Some((seed, f1, f2))
 }
 
 fn find_seeds(
     iv1: IV,
     iv2: IV,
-    frame_range1: (Frame, Frame),       // right-inclusive
-    frame_range2: (Frame, Frame),       // right-inclusive
+    frame_range1: (Frame, Frame),       // right-exclusive
+    frame_range2: (Frame, Frame),       // right-exclusive
     (seed_min, seed_max): (Seed, Seed), // right-inclusive
 ) -> Vec<(Seed, Frame, Frame)> {
+    let bmb1 = BMByte::from(vec![iv1.0, iv1.1, iv1.2, iv1.3, iv1.4, iv1.5]).unwrap();
+    let bmb2 = BMByte::from(vec![iv2.0, iv2.1, iv2.2, iv2.3, iv2.4, iv2.5]).unwrap();
+
     (seed_min..=seed_max)
         .into_par_iter()
         .progress_count((seed_max - seed_min + 1) as u64)
-        .flat_map(|seed| find_frame_pair(seed, iv1, iv2, frame_range1, frame_range2))
+        .flat_map(|seed| find_frame_pair(seed, &bmb1, &bmb2, frame_range1, frame_range2))
         .collect()
 }
 
@@ -76,8 +74,8 @@ fn main() {
         (0x33000000, 0x34000000),
     );
     for (seed, frame1, frame2) in result {
-        println!("{:x} {} {}", seed, frame1, frame2);
+        println!("Seed: {:x}, Frame1: {}, Frame2: {}", seed, frame1, frame2);
     }
 
-    println!("{:?}", now.elapsed().as_secs())
+    println!("Elapsed: {:?}", now.elapsed())
 }
