0a2b6c894b69d954af6228f02645eb10a8f26ff9
ukikagi <ukikagi@gmail.com>
2023-09-13 02:24:53 +0900

    More refactor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![feature(portable_simd)]
 mod multi_mt;
 
-use std::simd::{u32x8, SimdPartialEq};
-
 use indicatif::ParallelProgressIterator;
 use multi_mt::MultiMT19937;
 use rayon::prelude::*;
+use std::simd::{u32x8, Simd, SimdPartialEq};
 
 //  Misc consumption 60 + 1 + EC + PID
 const PRE_ADVANCE_FRAME: usize = 63;
@@ -26,17 +25,17 @@ fn find_frame(
     iv: u32,
     (frame_l, frame_r): (Frame, Frame), // right-exclusive
 ) -> u32x8 {
-    let mut curr = (mt.next_u5x8() << u32x8::splat(20))
-        | (mt.next_u5x8() << u32x8::splat(15))
-        | (mt.next_u5x8() << u32x8::splat(10))
-        | (mt.next_u5x8() << u32x8::splat(5))
+    let mut curr = (mt.next_u5x8() << Simd::splat(20))
+        | (mt.next_u5x8() << Simd::splat(15))
+        | (mt.next_u5x8() << Simd::splat(10))
+        | (mt.next_u5x8() << Simd::splat(5))
         | mt.next_u5x8();
-    let mut frame: u32x8 = u32x8::splat(0);
+    let mut frame: u32x8 = Simd::splat(0);
     for i in frame_l..frame_r {
-        curr = (curr & u32x8::splat(0x1FFFFFF)) << u32x8::splat(5) | mt.next_u5x8();
+        curr = (curr & Simd::splat(0x1FFFFFF)) << Simd::splat(5) | mt.next_u5x8();
         frame = curr
-            .simd_eq(u32x8::splat(iv))
-            .select(u32x8::splat(i as u32), frame);
+            .simd_eq(Simd::splat(iv))
+            .select(Simd::splat(i as u32), frame);
     }
     frame
 }
@@ -48,22 +47,15 @@ fn find_frame_pair(
     (f1_l, f1_r): (Frame, Frame), // right-exclusive
     (f2_l, f2_r): (Frame, Frame), // right-exclusive
 ) -> Vec<(Seed, Frame, Frame)> {
-    let seed_begin = seed_hi29 << 3;
-    let mut mt = MultiMT19937::from_seed(u32x8::from_array([
-        seed_begin,
-        seed_begin | 1,
-        seed_begin | 2,
-        seed_begin | 3,
-        seed_begin | 4,
-        seed_begin | 5,
-        seed_begin | 6,
-        seed_begin | 7,
-    ]));
+    let s = seed_hi29 << 3;
+    let seed = Simd::from_array([s, s | 1, s | 2, s | 3, s | 4, s | 5, s | 6, s | 7]);
+    let mut mt = MultiMT19937::from_seed(seed);
+
     mt.discard(PRE_ADVANCE_FRAME + f1_l);
 
     // Advances f1_r + 5 - f1_l frames
     let f1 = find_frame(&mut mt, iv1, (f1_l, f1_r));
-    if f1.simd_eq(u32x8::splat(0)).all() {
+    if f1.simd_eq(Simd::splat(0)).all() {
         return vec![];
     }
 
@@ -75,18 +67,18 @@ fn find_frame_pair(
     let mut results = Vec::new();
     for i in 0..8 {
         if f1[i] != 0 && f2[i] != 0 {
-            results.push((seed_begin | (i as u32), f1[i] as usize, f2[i] as usize));
+            results.push((s | (i as u32), f1[i] as usize, f2[i] as usize));
         }
     }
     results
 }
 
 fn find_seeds(
+    (seed_min, seed_max): (u32, u32), // right-inclusive
     iv1: IV,
     iv2: IV,
-    frame_range1: (Frame, Frame),     // right-exclusive
-    frame_range2: (Frame, Frame),     // right-exclusive
-    (seed_min, seed_max): (u32, u32), // right-inclusive
+    frame_range1: (Frame, Frame), // right-exclusive
+    frame_range2: (Frame, Frame), // right-exclusive
 ) -> Vec<(Seed, Frame, Frame)> {
     let iv1 = iv_to_u32(iv1);
     let iv2 = iv_to_u32(iv2);
@@ -106,11 +98,11 @@ fn main() {
     let now = std::time::Instant::now();
 
     let result = find_seeds(
+        (0x30000000, 0x40000000),
         (3, 5, 26, 31, 6, 19),
         (22, 27, 22, 1, 7, 27),
         (600, 800),
         (1500, 1700),
-        (0x30000000, 0x40000000),
     );
     println!("Completed!");
     println!("Elapsed: {:?}", now.elapsed());
--- src/multi_mt.rs
+++ src/multi_mt.rs
@@ -1,17 +1,15 @@
-use mersenne_twister::MT19937;
-use rand::{Rng, SeedableRng};
-use std::simd::u32x8;
+use std::simd::{u32x8, Simd};
 
 const N: usize = 624;
 const M: usize = 397;
-const ONE: u32x8 = u32x8::from_array([1, 1, 1, 1, 1, 1, 1, 1]);
-const MATRIX_A: u32x8 = u32x8::from_array([
+const ONE: u32x8 = Simd::from_array([1, 1, 1, 1, 1, 1, 1, 1]);
+const MATRIX_A: u32x8 = Simd::from_array([
     0x9908b0df, 0x9908b0df, 0x9908b0df, 0x9908b0df, 0x9908b0df, 0x9908b0df, 0x9908b0df, 0x9908b0df,
 ]);
-const UPPER_MASK: u32x8 = u32x8::from_array([
+const UPPER_MASK: u32x8 = Simd::from_array([
     0x80000000, 0x80000000, 0x80000000, 0x80000000, 0x80000000, 0x80000000, 0x80000000, 0x80000000,
 ]);
-const LOWER_MASK: u32x8 = u32x8::from_array([
+const LOWER_MASK: u32x8 = Simd::from_array([
     0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff,
 ]);
 
@@ -22,7 +20,7 @@ pub struct MultiMT19937 {
 
 const UNINITIALIZED: MultiMT19937 = MultiMT19937 {
     idx: 0,
-    state: [u32x8::from_array([0, 0, 0, 0, 0, 0, 0, 0]); N],
+    state: [Simd::from_array([0, 0, 0, 0, 0, 0, 0, 0]); N],
 };
 
 impl MultiMT19937 {
@@ -37,9 +35,9 @@ impl MultiMT19937 {
         self.idx = N;
         self.state[0] = seed;
         for i in 1..N {
-            self.state[i] = u32x8::splat(1812433253)
-                * (self.state[i - 1] ^ (self.state[i - 1] >> u32x8::splat(30)))
-                + u32x8::splat(i as u32);
+            self.state[i] = Simd::splat(1812433253)
+                * (self.state[i - 1] ^ (self.state[i - 1] >> Simd::splat(30)))
+                + Simd::splat(i as u32);
         }
     }
 
@@ -56,7 +54,7 @@ impl MultiMT19937 {
 
     #[inline]
     pub fn next_u5x8(&mut self) -> u32x8 {
-        self.next_u32x8() >> u32x8::splat(27)
+        self.next_u32x8() >> Simd::splat(27)
     }
 
     #[inline]
@@ -69,47 +67,39 @@ impl MultiMT19937 {
     fn fill_next_state(&mut self) {
         for i in 0..N - M {
             let x = (self.state[i] & UPPER_MASK) | (self.state[i + 1] & LOWER_MASK);
-            self.state[i] = self.state[i + M] ^ (x >> u32x8::splat(1)) ^ ((x & ONE) * MATRIX_A);
+            self.state[i] = self.state[i + M] ^ (x >> Simd::splat(1)) ^ ((x & ONE) * MATRIX_A);
         }
         for i in N - M..N - 1 {
             let x = (self.state[i] & UPPER_MASK) | (self.state[i + 1] & LOWER_MASK);
-            self.state[i] = self.state[i + M - N] ^ (x >> u32x8::splat(1)) ^ ((x & ONE) * MATRIX_A);
+            self.state[i] = self.state[i + M - N] ^ (x >> Simd::splat(1)) ^ ((x & ONE) * MATRIX_A);
         }
         let x = (self.state[N - 1] & UPPER_MASK) | (self.state[0] & LOWER_MASK);
-        self.state[N - 1] = self.state[M - 1] ^ (x >> u32x8::splat(1)) ^ ((x & ONE) * MATRIX_A);
+        self.state[N - 1] = self.state[M - 1] ^ (x >> Simd::splat(1)) ^ ((x & ONE) * MATRIX_A);
         self.idx = 0;
     }
 }
 
 #[inline]
 fn temper(mut x: u32x8) -> u32x8 {
-    x ^= x >> u32x8::splat(11);
-    x ^= (x << u32x8::splat(7)) & u32x8::splat(0x9d2c5680);
-    x ^= (x << u32x8::splat(15)) & u32x8::splat(0xefc60000);
-    x ^= x >> u32x8::splat(18);
+    x ^= x >> Simd::splat(11);
+    x ^= (x << Simd::splat(7)) & Simd::splat(0x9d2c5680);
+    x ^= (x << Simd::splat(15)) & Simd::splat(0xefc60000);
+    x ^= x >> Simd::splat(18);
     x
 }
 
 #[test]
 fn test_compare_with_mt() {
-    let mut mts: Vec<MT19937> = Vec::new();
-    let mut vec: Vec<u32> = Vec::new();
-    for i in 0..8 {
-        mts.push(MT19937::from_seed(i as u32));
-        for _ in 0..1000 {
-            mts[i].next_u32();
-        }
-        vec.push(mts[i].next_u32());
-    }
-
     let mut multi_mt: MultiMT19937 =
-        MultiMT19937::from_seed(u32x8::from_array([0, 1, 2, 3, 4, 5, 6, 7]));
+        MultiMT19937::from_seed(Simd::from_array([0, 1, 2, 3, 4, 5, 6, 7]));
     for _ in 0..1000 {
         multi_mt.next_u32x8();
     }
-
     assert_eq!(
         multi_mt.next_u32x8().to_array(),
-        [vec[0], vec[1], vec[2], vec[3], vec[4], vec[5], vec[6], vec[7]]
+        [
+            1333075495, 375733240, 1144994631, 454162887, 3777932409, 3818223146, 3836374258,
+            4142999817
+        ]
     );
 }
